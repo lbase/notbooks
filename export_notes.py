55a83364a935ac1ce948305774ed4f76a165902d
rfile <lbase@finormile.com>
2018-03-11 12:20:09 -0400

    switch to pandas to parse

--- export_notes.py
+++ export_notes.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 from sys import argv
 import os
-import csv
+import pandas as pd
 from slugify import slugify
+
 
 def export_notes(filename, savepath):
     filext='.txt'
     if os.path.exists(savepath) == False :
         os.mkdir(savepath)
-    with open(filename, newline = '') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter='"')
-        for row in reader:
-            if row['Title'] is None :
-                row['Title'] = 'title text'
-            if row['Text'] is None :
-               row['Text'] = 'no text'
+    gq = open(filename, 'r') 
+    reader = pd.read_csv(gq)
+    for index, row in reader.iterrows():
+            
+            
             fullfilename = slugify(row['Title'], separator=' ') + filext
+            # fullfilename = (row['Title']) + filext
             newfilename=os.path.join(savepath, fullfilename)
             revnote = open(newfilename, 'w')
-            revnote.write(row['Text'].replace('\\n','\n'))
-            revnote.close
- 
- 
+            #revnote.write(row['Text'].replace('\\n','\n'))
+            revnote.write(row['Text'])
+            revnote.close    
+        
 
 export_notes(argv[1] , argv[2])
 
@@ -42,5 +42,9 @@ going to get rid of most of the spurious data before I commit...
 3-10-18
 
 rfile at home
-
+==========
+3-11-18
+switch to pandas 
+works with out double import export
+mdb-export GeoQuick.dat GeoQuick_Tb > gqmdb.csv
 """
