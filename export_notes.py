6bdc6c149d2b574cef114ddf3a6371648659a5a6
rfile <lbase@finormile.com>
2018-03-14 11:11:56 -0400

    pandas

--- export_notes.py
+++ export_notes.py
@@ -4,39 +4,47 @@ import os
 import pandas as pd
 from slugify import slugify
 
-import pandas as pd
 
+def export_notes(filename, savepath):
+    filext='.txt'
+    if os.path.exists(savepath) == False :
+        os.mkdir(savepath)
+    gq = open(filename, 'r') 
+    reader = pd.read_csv(gq)
+    for index, row in reader.iterrows():
+            
+            
+            fullfilename = slugify(row['Title'], separator=' ') + filext
+            # fullfilename = (row['Title']) + filext
+            newfilename=os.path.join(savepath, fullfilename)
+            revnote = open(newfilename, 'w')
+            #revnote.write(row['Text'].replace('\\n','\n'))
+            revnote.write(row['Text'])
+            revnote.close    
+        
 
-def switch_demo(argument):
-    switcher = {
-        0: "Main",
-        1: "Menu 1",
-        2: "Menu 2",
-        3: "Menu 3",
-        4: "Menu 4",
-        5: "Menu 5",
-        6: "Menu 6",
-        7: "Menu 7",
-        8: "Menu 8",
-        9: "Menu 9",
-        10: "Menu 10",
-        11: "Menu 11",
-        12: "Menu 12",
-        13: "Menu 13",
-        14: "Menu 14",
-        15: "Menu 15"
+export_notes(argv[1] , argv[2])
 
-    }
-    return switcher.get(argument, "Invalid month")
+"""
+gmdb2 did not seem to do the trick on export. 
 
+Had to do a double import export
 
-msqlfile = open('gqmdb.csv', 'r')
-notes = pd.read_csv(msqlfile)
-print(notes['Title'][8])
-print(notes['Text'][8])
-print(notes['Menu_Number'][8])
-menu_entry = (notes['Menu_Number'][8])
+ended up exporting sql and then importing into sqlite db and exporting again
 
-print(switch_demo(menu_entry))
-mycsvfile = open(mymenu.csv, 'w')
-menunotes = pd.to_csv
+note the delimiter line for the DictReader as the export from sqlite was single quote double quote
+
+```
+mdb-export -I sqlite GeoQuick.dat GeoQuick_Tb > geoquick.sql
+```
+going to get rid of most of the spurious data before I commit...
+
+3-10-18
+
+rfile at home
+==========
+3-11-18
+switch to pandas 
+works with out double import export
+mdb-export GeoQuick.dat GeoQuick_Tb > gqmdb.csv
+"""
