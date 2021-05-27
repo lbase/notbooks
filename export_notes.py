8ddcec12518e7351fcd46d22e151e0e8c8399761
rfile <lbase@finormile.com>
2021-05-27 13:35:48 -0400

    new work

--- export_notes.py
+++ export_notes.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-import os
-import pandas as pd
-from slugify import slugify
+from lxml import etree
 
-
-def export_notes(filename, savepath):
-    filext='.txt'
-    if os.path.exists(savepath) == False :
-        os.mkdir(savepath)
-    gq = open(filename, 'r') 
-    reader = pd.read_csv(gq)
-    for index, row in reader.iterrows():
-            
-            
-            fullfilename = slugify(row['Title'], separator=' ') + filext
-            # fullfilename = (row['Title']) + filext
-            newfilename=os.path.join(savepath, fullfilename)
-            revnote = open(newfilename, 'w')
-            #revnote.write(row['Text'].replace('\\n','\n'))
-            revnote.write(row['Text'])
-            revnote.close    
-        
-
-export_notes(argv[1] , argv[2])
-
+NSM = {"gpx": "http://www.topografix.com/GPX/1/0"}
+NSGS = {"groundspeak": "http://www.groundspeak.com/cache/1/0/1"}
+tree = etree.parse("CCreek.gpx")
+root = tree.getroot()
+# for elem in tree.findall("//wpt/type", root.nsmap):
+map2={'gpx': 'http://www.topografix.com/GPX/1/0','groundspeak': 'http://www.groundspeak.com/cache/1/0/1'}
+#>>> root.xpath('//a:HistoryRecords', namespaces=nsmap)
+for elem in root.findall("groundspeak:type", NSGS):
+    print(elem)
 
